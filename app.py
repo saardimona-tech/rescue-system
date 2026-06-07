48083d05b4702ba670cf1bd3b2c4a2a6724feb45
saardimona-tech <saardimona@gmail.com>
2026-06-07 13:35:44 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,12 +1,85 @@
-from flask import Flask
+from flask import Flask, request
 
 app = Flask(__name__)
 
+TEAMS = [
+    "צוות 1",
+    "צוות 2",
+    "צוות 3",
+    "צוות רפואה",
+    "מודיעין אוכלוסייה",
+    "צוות פיקוד"
+]
+
+users = []
+
 @app.route("/")
 def home():
-    return """
-    <h1>מערכת חילוץ</h1>
-    <p>האתר עובד 🚑</p>
+    team_options = ""
+
+    for team in TEAMS:
+        team_options += f'<option value="{team}">{team}</option>'
+
+    return f"""
+    <html dir="rtl">
+    <body style="font-family:Arial;text-align:center;margin-top:50px">
+
+    <h1>מערכת חילוץ 🚑</h1>
+
+    <form action="/register" method="post">
+
+        <input
+            type="text"
+            name="name"
+            placeholder="שם מלא"
+            required
+        >
+
+        <br><br>
+
+        <select name="team">
+            {team_options}
+        </select>
+
+        <br><br>
+
+        <button type="submit">
+            כניסה
+        </button>
+
+    </form>
+
+    </body>
+    </html>
+    """
+
+@app.route("/register", methods=["POST"])
+def register():
+    name = request.form["name"]
+    team = request.form["team"]
+
+    users.append({
+        "name": name,
+        "team": team,
+        "status": False
+    })
+
+    return f"""
+    <html dir="rtl">
+    <body style="font-family:Arial;text-align:center;margin-top:50px">
+
+    <h2>שלום {name}</h2>
+
+    <p>צוות: {team}</p>
+
+    <button>✅ הגעתי לזירה</button>
+
+    <br><br>
+
+    <button>❌ יצאתי מהזירה</button>
+
+    </body>
+    </html>
     """
 
 if __name__ == "__main__":
